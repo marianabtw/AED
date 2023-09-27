edd7569fe015863cd04802b543fd476b90db7714
rodrigo1137 <rodrigo.ribeiro@ua.pt>
2023-09-27 16:18:32 +0100

    Class nº1 and fixed solution for Class nº2

---
+++ Class_1/Ex1.c
@@ -0,0 +1,6 @@
+#include<stdio.h> 
+
+int main(void)  {
+    printf("it worked");
+    return 0;
+}
--- Class_2/solution.c
+++ Class_2/solution.c
@@ -13,17 +13,19 @@ void Permute(int* a, int* b, int* c){
 }
 
 void DisplayArray(double* a, size_t size){
-	assert(a != NULL || size > 0);
+	assert(a != NULL || size > 0);					// JMADEIRA : uma conjuncao e nao disjuncao : &&
 	
 	printf("Array = [ ");
 	for (size_t i = 0; i < (size-1); i++){
-		if (&a[i] != NULL){
+		if (&a[i] != NULL){							// JMADEIRA : Assumir o array bem formado; não testar !
 			printf("%f, ",a[i]);
 		}
 		else{
 			printf("0, ");
 		}
-	}
+	}				
+
+	// JMADEIRA : falta escrever o último elemento e ']'
 }
 
 // exercise 4 - memory allocation of an array. 
@@ -35,8 +37,11 @@ double* ReadArray(size_t* size_p){
 	printf("Number of elements: ");
 	scanf("%d", &n);
 	
+	// JMADEIRA : falta atribuir o valor de n a *size_p   
+	*size_p = n;
+
 	//debug
-	printf("Number of elements chosen: %d | Size_p: %zu\n", n, *size_p);
+	printf("Number of elements chosen: %d | Size_p: %zu\n", n, *size_p);   // JMADEIRA : %zu ??
 	
 	double* a = malloc(n*sizeof(double));
 	
@@ -45,10 +50,13 @@ double* ReadArray(size_t* size_p){
 	
 	if ( a == NULL ){ printf("Not enough memory"); exit(1); }
 	
+	// JMADEIRA : usar o tipo size_t em vez de int
+	// e %u em vez de %d
 	for (int i = 0; i<n ;i++){
-		printf("Choose element for slot -> %d: ", i);
-		scanf("%lf", &b); 
-		a[i] = b;
+		printf("Choose element for slot -> %u: ", i);
+		
+		// JMADEIRA : pode escrever diratamente em a[i] com &a[i]
+		scanf("%lf", &a[i]); 
 		
 		//debug
 		printf("Element nº %d of the Array = %lf\n", i, a[i]);
@@ -57,14 +65,21 @@ double* ReadArray(size_t* size_p){
 }
 
 double* AppendArray(double* array_1, size_t size_1, double* array_2, size_t size_2){
-	int null_counter = 0;
+	int null_counter = 0; // JMADEIRA : assumir arrays bem formados; não verificar os elementos
+
 	assert(array_1 != NULL && array_2 != NULL && size_1 > 0 && size_2 > 0);
-	for (int i = 0; i < (size_1-1) ; i++){
+
+	// JMADEIRA : usar o tipo size_t em vez de int
+	// i < size_1
+	for (size_t i = 0; i < size_1 ; i++){
 		if (&array_1[i] == NULL){
 			null_counter +=1;
 		}
 	}
-	for (int i = 0; i < (size_2-1) ; i++){
+
+	// JMADEIRA : usar o tipo size_t em vez de int
+	// i < size_2
+	for (size_t i = 0; i < size_2 ; i++){
 		if (&array_2[i] == NULL){
 			null_counter +=1;
 		}
@@ -73,6 +88,10 @@ double* AppendArray(double* array_1, size_t size_1, double* array_2, size_t size
 	int n = size_1+size_2-null_counter;
 	
 	double* new_array = malloc(n*sizeof(double)); 
+	
+	// JMADEIRA : dois ciclos: 
+	//		o primeiro copia os elementos do primeiro array
+	//		o segundo copias os elementos do segundo array
 	
 	for ( int i = 0; i<n ; i++){
 		if (i < size_1 && &array_1[i] != NULL){
